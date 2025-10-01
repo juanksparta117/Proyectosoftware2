ed1be081845eafcfad90450b37d8d525dc2a67a1
juanksparta117 <juanksparta@gmail.com>
2025-09-30 23:51:12 -0500

    se adelanto otro poquito

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import express from 'express';
-
+import {createpool} from  'mysql2/promise'
 const app = express();
+const pool = createpool({
+  host: 'localhost',
+  user: 'root',
+  password: "123456",
+  port: 3307
+
+})
 
 app.get('/', (req, res) => {
   res.send('Hello World');
-}
-);
+});
+
+app.get('/ping', async (req, res) => {
+  const result = await pool.query('SELECT NOW()')
+  res.json(result[0]);
+});
 
 
 app.listen(3000)
